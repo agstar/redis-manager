b99c88e4eefe3af945615c3ff936b69402cd9c26
agstar <2541415200@qq.com>
2018-12-10 15:30:26 +0800

    项目基础框架

---
+++ src/main/java/com/redis/redismanage/RedisManageApplication.java
@@ -0,0 +1,12 @@
+package com.redis.redismanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisManageApplication.class, args);
+    }
+}
