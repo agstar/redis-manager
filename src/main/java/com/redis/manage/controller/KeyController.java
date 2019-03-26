d52b47cbe93bb8d81fcd69a3083b170fefdf4e55
agstar <2541415200@qq.com>
2019-03-26 10:34:23 +0800

    优化key显示

--- src/main/java/com/redis/manage/controller/KeyController.java
+++ src/main/java/com/redis/manage/controller/KeyController.java
@@ -5,6 +5,7 @@ import com.redis.manage.entity.Result;
 import com.redis.manage.entity.StatusCode;
 import com.redis.manage.model.RedisServer;
 import com.redis.manage.util.RedisServerUtil;
+import org.springframework.data.redis.connection.DataType;
 import org.springframework.data.redis.core.Cursor;
 import org.springframework.data.redis.core.RedisCallback;
 import org.springframework.data.redis.core.ScanOptions;
@@ -22,7 +23,7 @@ public class KeyController {
     private static final ScanOptions SCAN_OPTIONS = new ScanOptions.ScanOptionsBuilder().match("*").count(10000).build();
 
     @GetMapping("key/{serverName}/{dbIndex}")
-    public Result getKey(@PathVariable("serverName") String serverName, @PathVariable("dbIndex") int dbIndex) {
+    public Result getKeyList(@PathVariable("serverName") String serverName, @PathVariable("dbIndex") int dbIndex) {
         StringRedisTemplate stringRedisTemplate = REDIS_TEMPLATE_MAP.get(serverName + DEFAULT_SEPARATOR + dbIndex);
         if (stringRedisTemplate == null) {
             Optional<RedisServer> first = REDIS_SERVER.stream().filter(x -> x.getName().equals(serverName)).findFirst();
@@ -33,6 +34,60 @@ public class KeyController {
             }
         }
         //scan 0 MATCH * COUNT 10000
+        //加载初始化 template，点击server加载template
+        Set<String> keys = stringRedisTemplate.execute((RedisCallback<Set<String>>) redisConnection -> {
+            Set<String> binaryKeys = new HashSet<>();
+            Cursor<byte[]> cursor = redisConnection.scan(SCAN_OPTIONS);
+            while (cursor.hasNext()) {
+                binaryKeys.add(new String(cursor.next()));
+            }
+            return binaryKeys;
+        });
+        List<JSONObject> list = new ArrayList<>();
+        if (keys != null) {
+            keys.forEach(x -> list.add(RedisServerUtil.getKeyTree(x)));
+        }
+        return new Result(true, StatusCode.OK, "查询成功", list);
+    }
+
+
+    @GetMapping("key/{serverName}/{dbIndex}/{keyName}")
+    public Result getKey(@PathVariable("serverName") String serverName, @PathVariable("dbIndex") int dbIndex, @PathVariable("keyName") String keyName) {
+        StringRedisTemplate stringRedisTemplate = REDIS_TEMPLATE_MAP.get(serverName + DEFAULT_SEPARATOR + dbIndex);
+        if (stringRedisTemplate == null) {
+            Optional<RedisServer> first = REDIS_SERVER.stream().filter(x -> x.getName().equals(serverName)).findFirst();
+            if (first.isPresent()) {
+                stringRedisTemplate = RedisServerUtil.initRedisConnection(first.get(), dbIndex);
+            } else {
+                return new Result(false, StatusCode.ERROR, "没有对应的服务");
+            }
+        }
+        DataType type = stringRedisTemplate.type(keyName);
+
+        if (type != null) {
+            switch (type) {
+                case SET:
+                    stringRedisTemplate.opsForValue().get(keyName);
+                    break;
+                case HASH:
+                    Map<Object, Object> entries = stringRedisTemplate.opsForHash().entries(keyName);
+                    break;
+                case LIST:
+                    List<String> range = stringRedisTemplate.opsForList().range(keyName, 0, 1000);
+                    break;
+                case STRING:
+                    String s = stringRedisTemplate.opsForValue().get(keyName);
+                    break;
+                case NONE:
+                    break;
+                case ZSET:
+                    Set<String> range1 = stringRedisTemplate.opsForZSet().range(keyName, 0, 1000);
+                    break;
+                default:
+                    break;
+            }
+        }
+
         //加载初始化 template，点击server加载template
         Set<String> keys = stringRedisTemplate.execute((RedisCallback<Set<String>>) redisConnection -> {
             Set<String> binaryKeys = new HashSet<>();
